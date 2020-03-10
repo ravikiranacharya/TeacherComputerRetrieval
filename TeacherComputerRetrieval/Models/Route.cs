e8adf04a26099e1951fa10cf5c68f63dc407c87b
Ravi Acharya <raviraaga1992@gmail.com>
2020-03-10 19:53:01 +0530

    Created Route class

---
+++ TeacherComputerRetrieval/Models/Route.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherComputerRetrieval.Models
+{
+    class Route
+    {
+        private string StartingAcademy { get; set; }
+        private string EndingAcademy { get; set; }
+        private int Distance { get; set; }
+
+        public Route(string startingAcademy, string endingAcademy, int distance)
+        {
+            this.StartingAcademy = startingAcademy;
+            this.EndingAcademy = endingAcademy;
+            this.Distance = distance;
+        }
+
+
+    }
+}
