b699d7fd7f97a03ad4bdea11330dae906013f8be
Ravi Acharya <raviraaga1992@gmail.com>
2020-03-10 20:01:59 +0530

    Created Journey class and added sample input to Main

---
+++ TeacherComputerRetrieval/Models/Journey.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherComputerRetrieval.Models
+{
+    class Journey
+    {
+        private List<string> Stops { get; set; }
+
+        public Journey()
+        {
+            this.Stops = new List<string>();
+        }
+    }
+}
--- TeacherComputerRetrieval/Program.cs
+++ TeacherComputerRetrieval/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeacherComputerRetrieval.Models;
 
 namespace TeacherComputerRetrieval
 {
@@ -10,6 +11,18 @@ namespace TeacherComputerRetrieval
     {
         static void Main(string[] args)
         {
+            List<Route> routes = new List<Route>();
+            routes.Add(new Route("A", "B", 5));
+            routes.Add(new Route("B", "C", 4));
+            routes.Add(new Route("C", "D", 8));
+            routes.Add(new Route("D", "C", 8));
+            routes.Add(new Route("D", "E", 6));
+            routes.Add(new Route("A", "D", 5));
+            routes.Add(new Route("C", "E", 2));
+            routes.Add(new Route("E", "B", 3));
+            routes.Add(new Route("A", "E", 7));
+
+            Console.WriteLine("Hello world");
         }
     }
 }
